94bb304201d1fcd60264cc525143d6452caf9a51
leegwichan <cnddkscndgus@naver.com>
2025-09-30 14:36:55 +0900

    test: matplotlib 을 이용한 시각화

---
+++ test/matplotlib_example.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    # 좌표 리스트 (Numpy 배열 형태로 준비)
+    data = np.array([(0.01, 0.02, 0.03), (0.02, 0.01, -0.01), (-0.03, 0.00, 0.02), (0.00, 0.03, 0.01)])
+    X, Y, Z = data[:, 0], data[:, 1], data[:, 2]
+
+    # 3D 축 생성
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 3D 산점도 그리기
+    ax.scatter(X, Y, Z, c='blue', marker='o')
+
+    # 축 레이블 설정
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+    ax.set_title('Particle Positions (Matplotlib 3D)')
+
+    plt.show()
